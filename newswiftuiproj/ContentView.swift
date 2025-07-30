e0891f2514c041b61b9be7a2b6392b4869d563b5
vikas <vikas.s@bystakk.com>
2025-07-30 11:12:40 +0530

    Initial Commit

---
+++ newswiftuiproj/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  newswiftuiproj
+//
+//  Created by Sequoia on 30/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ newswiftuiproj/newswiftuiprojApp.swift
@@ -0,0 +1,17 @@
+//
+//  newswiftuiprojApp.swift
+//  newswiftuiproj
+//
+//  Created by Sequoia on 30/07/25.
+//
+
+import SwiftUI
+
+@main
+struct newswiftuiprojApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
